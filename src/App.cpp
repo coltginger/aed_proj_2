99a74543966f1efa0f255fb4dfefc4583710926c
coltginger <gnbarroso@gmail.com>
2023-12-28 15:21:11 +0000

    Part of app

--- src/App.cpp
+++ src/App.cpp
@@ -1,7 +1,16 @@
 #include "App.h"
+#include <iostream>
+#include <string>
+using namespace std;
 
 void App::run() {
-    _worldGraph.makeAirports();
+    bool STOP = true;
+
+    while (STOP){
+        cout << "1. Show the global number of airports and number of flights." << endl
+             << "2. Number of flights out of an airport and from how many different airlines." << endl
+             << "3. Number of flights per city or airline" << endl;
+    }
 }
 
 App::App() {}
