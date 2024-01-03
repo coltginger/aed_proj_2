5fa1a62f2117cb3db92d1d54c6aebcda810454ad
coltginger <gnbarroso@gmail.com>
2024-01-03 00:52:11 +0000

    Final commit

--- docs/html/menudata.js
+++ docs/html/menudata.js
@@ -24,31 +24,4 @@
 */
 var menudata={children:[
 {text:"Main Page",url:"index.html"},
-{text:"Classes",url:"annotated.html",children:[
-{text:"Class List",url:"annotated.html"},
-{text:"Class Index",url:"classes.html"},
-{text:"Class Members",url:"functions.html",children:[
-{text:"All",url:"functions.html",children:[
-{text:"a",url:"functions.html#index_a"},
-{text:"b",url:"functions.html#index_b"},
-{text:"f",url:"functions.html#index_f"},
-{text:"g",url:"functions.html#index_g"},
-{text:"m",url:"functions.html#index_m"},
-{text:"n",url:"functions.html#index_n"},
-{text:"o",url:"functions.html#index_o"},
-{text:"r",url:"functions.html#index_r"},
-{text:"s",url:"functions.html#index_s"},
-{text:"w",url:"functions.html#index_w"}]},
-{text:"Functions",url:"functions_func.html",children:[
-{text:"a",url:"functions_func.html#index_a"},
-{text:"b",url:"functions_func.html#index_b"},
-{text:"f",url:"functions_func.html#index_f"},
-{text:"g",url:"functions_func.html#index_g"},
-{text:"m",url:"functions_func.html#index_m"},
-{text:"n",url:"functions_func.html#index_n"},
-{text:"o",url:"functions_func.html#index_o"},
-{text:"r",url:"functions_func.html#index_r"},
-{text:"s",url:"functions_func.html#index_s"},
-{text:"w",url:"functions_func.html#index_w"}]}]}]},
-{text:"Files",url:"files.html",children:[
-{text:"File List",url:"files.html"}]}]}
+{text:"Related Pages",url:"pages.html"}]}
--- docs/html/search/searchdata.js
+++ docs/html/search/searchdata.js
@@ -1,21 +1,18 @@
 var indexSectionsWithContent =
 {
-  0: "abcefgmnorsvw",
-  1: "acefgvw",
-  2: "abfgmnorsw"
+  0: "a",
+  1: "a"
 };
 
 var indexSectionNames =
 {
   0: "all",
-  1: "classes",
-  2: "functions"
+  1: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "All",
-  1: "Classes",
-  2: "Functions"
+  1: "Pages"
 };
 
